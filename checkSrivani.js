829e5f58696cc33644f2187c903d9a54cba8725d
Raghu Krishnan <raghu.krishnan@photoninfotech.net>
2025-07-08 18:43:12 +0530

    Update checkSrivani.js

--- checkSrivani.js
+++ checkSrivani.js
@@ -3,22 +3,19 @@ const fs = require('fs');
 
 (async () => {
   const browser = await puppeteer.launch({
-  headless: "new", // or true
-  executablePath: '/usr/bin/chromium-browser',
-  args: ['--no-sandbox']
+    headless: 'new',
+    executablePath: '/usr/bin/chromium-browser',
+    args: ['--no-sandbox']
   });
 
-
   const page = await browser.newPage();
-
   await page.goto('https://webapps.tirumala.org/SrivaniTokenLiveTV/LiveStatus.aspx', { waitUntil: 'domcontentloaded' });
-  await page.waitForTimeout(5000); // Wait for page content to load
+  await page.waitForTimeout(5000);
 
   const dateTime = await page.$eval('#lblCurrentDate', el => el.innerText).catch(() => 'N/A');
   const availableTickets = await page.$eval('#lblAvailableQuota', el => el.innerText).catch(() => 'N/A');
 
   console.log(`Date: ${dateTime}, Tickets: ${availableTickets}`);
-
   const csvLine = `"${dateTime}","${availableTickets}"\n`;
   fs.appendFileSync('srivani.csv', csvLine);
 
